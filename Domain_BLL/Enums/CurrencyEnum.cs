62ffd91d5314a3dbdf86029657bc9d11a50df581
Nnamani Stephen <sir.stephennnamani@gmail.com>
2023-02-23 05:30:18 +0100

    Currency Enumeration Added

---
+++ Domain_BLL/Enums/CurrencyEnum.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain_BLL.Enums
+{
+    enum CurrencyEnum
+    {
+        Naira,
+        Dollar,
+        Euro,
+        Pounds,
+        Shelling,
+        Kobo,
+        
+    }
+}
